290096382e6b42f29d8b00496f6fdc673062b1bf
西山希 <nishiyama@beartail.jp>
2019-10-28 19:26:13 +0900

    [Add] greeter.test.jsを実装

---
+++ src/__tests__/greeter.test.ts
@@ -0,0 +1,22 @@
+class Student {
+  public fullName: string;
+  constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+    this.fullName = firstName + ' ' + middleInitial + ' ' + lastName;
+  }
+}
+
+interface Person {
+  firstName: string,
+  lastName: string,
+}
+
+
+function greeter(person: Person) {
+  return 'Hello, ' + person.firstName + ' ' + person.lastName;
+}
+
+test('greeter', () => {
+  const nozomit = new Student('Nozomi', 'N.', 'Nishiyama');
+  expect(nozomit.fullName).toEqual('Nozomi N. Nishiyama');
+  expect(greeter({ firstName: 'Nozomi', lastName: 'Nishiyama' })).toEqual('Hello, Nozomi Nishiyama');
+})
