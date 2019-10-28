6f0b2ffe22e807b15d6f86d04ad953c64bc1211f
Kitamura Satoshi <34638005+kitamura-beartail@users.noreply.github.com>
2019-10-28 18:47:48 +0900

    booleanについてサンプルコードを追加しました (#2)
    * Implement boolean sample code
    
    * Move to sub directory
    
    * Tidy up empty lines

---
+++ src/1-basic-types/kitamura/__tests__/boolean.test.ts
@@ -0,0 +1,20 @@
+function nyanEnabled(animal: string): boolean {
+  switch(animal) {
+    case 'cat':
+      return true;
+  }
+  return false;
+}
+
+test('flags', () => {
+  const isCat: boolean = true;
+  const isDog: boolean = false;
+  expect(isCat).toBe(true);
+  expect(isDog).toBe(false);
+});
+
+test('nyanEnabled', () => {
+  expect(nyanEnabled('cat')).toBe(true);
+  expect(nyanEnabled('dog')).toBe(false);
+  expect(nyanEnabled('cat person')).toBe(false);
+});
