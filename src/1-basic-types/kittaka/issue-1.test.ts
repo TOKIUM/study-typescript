a83b47ac9333930f5c5e5b32b92174faaecb552a
staka121 <staka121@users.noreply.github.com>
2019-10-29 18:29:58 +0900

    [fix] テストミス修正

--- src/1-basic-types/kittaka/issue-1.test.ts
+++ src/1-basic-types/kittaka/issue-1.test.ts
@@ -1,7 +1,7 @@
 import getProgress from './issue-1';
 
 test('getProgress', () => {
-  expect(getProgress(['*', '*', '*'], ['*', '*', '*'])).toBe(100);
+  expect(getProgress(['*', '*', '*'], ['*', '*', '*'])).toBe(50);
   expect(getProgress(['*', '*'], ['*', '*', '*'])).toBe(40);
   expect(getProgress(['*'], ['*', '*', '*'])).toBe(25);
 });
