c5f1426760b09bb64fb9b51b7cd9064000e803e6
Kitamura Satoshi <34638005+kitamura-beartail@users.noreply.github.com>
2019-10-29 18:49:24 +0900

    2019-10-29のお題 2問 (#4)
    * Implement getProgress
    
    * Implement containSearchText
    
    * More type
    
    * Move to test dir

---
+++ src/1-basic-types/kitamura/__tests__/containSearchText.test.ts
@@ -0,0 +1,57 @@
+// app/frontend/javascripts/applications/reports/components/ReportTable.jsx
+/**
+ * 検索文字列を含むプロジェクトを探す
+ * project.displayId が displayId を含む、もしくは project.name が name を含むか否か
+ * @param {string} displayId 文字列
+ * @param {string} name 文字列
+ * @param {Project} project Project
+ * @return {boolean}
+ */
+function containSearchText(displayId: string, name: string, project: Project) {
+  return (!displayId || (project.displayId.indexOf(displayId) > -1))
+    && (!name || (project.name && project.name.indexOf(name) > -1))
+}
+
+class Project {
+  displayId: string
+  name: string
+
+  constructor(displayId: string, name: string) {
+    this.displayId = displayId
+    this.name = name
+  }
+}
+
+describe('containSearchText', () => {
+  test('same displayId and name with the project', () => {
+    const displayId: string = 'a'
+    const name: string = 'A'
+    const project: Project = new Project(displayId, name)
+
+    expect(containSearchText(displayId, name, project)).toBe(true)
+  })
+
+  test('with different displayId', () => {
+    const displayId: string = 'a'
+    const name: string = 'A'
+    const project: Project = new Project('displayId', name)
+
+    expect(containSearchText(displayId, name, project)).toBe(true)
+  })
+
+  test('with different name', () => {
+    const displayId: string = 'a'
+    const name: string = 'A'
+    const project: Project = new Project(displayId, 'name')
+
+    expect(containSearchText(displayId, name, project)).toBe(false)
+  })
+
+  test('with different displayId and name', () => {
+    const displayId: string = 'a'
+    const name: string = 'A'
+    const project: Project = new Project('displayId', 'name')
+
+    expect(containSearchText(displayId, name, project)).toBe(false)
+  })
+})
