d43439f534122739f9d74e667502239d86dec040
Kitamura Satoshi <kitamura_s@beartail.jp>
2019-10-30 20:29:13 +0900

    Implement #1

---
+++ src/3-interfaces/kitamura/__tests__/handleStatusChange.test.ts
@@ -0,0 +1,50 @@
+// app/frontend/javascripts/applications/transactions/transaction_searchbox.jsx
+
+interface SearchCondition {
+  status?: string[]
+}
+
+class SearchBox {
+  searchCondition: SearchCondition
+
+  constructor() {
+    this.searchCondition = {
+      status: ['default', 'applied'] // default value
+    }
+  }
+
+  /**
+   * @param {boolean} checked checked or not
+   * @param {string} value input value
+   */
+  handleStatusChange(checked: boolean, value: string) {
+    const filtered: string[] = this.searchCondition.status.filter(x => x !== value || checked)
+    const additional: boolean = checked && filtered.indexOf(value) < 0
+    const status: string[] = additional ? [...filtered, value] : filtered
+    this.setSearchConditions({ status })
+  }
+
+  private setSearchConditions(searchCondition: SearchCondition) {
+    this.searchCondition = searchCondition
+  }
+}
+
+describe('handleStatusChange', () => {
+  test('check default', () => {
+    const searchBox: SearchBox = new SearchBox
+    searchBox.handleStatusChange(true, 'default')
+    expect(searchBox.searchCondition.status).toEqual(['default', 'applied'])
+  })
+
+  test('uncheck default', () => {
+    const searchBox: SearchBox = new SearchBox
+    searchBox.handleStatusChange(false, 'default')
+    expect(searchBox.searchCondition.status).toEqual(['applied'])
+  })
+
+  test('check nyan', () => {
+    const searchBox: SearchBox = new SearchBox
+    searchBox.handleStatusChange(true, 'nyan')
+    expect(searchBox.searchCondition.status).toEqual(['default', 'applied', 'nyan'])
+  })
+})
