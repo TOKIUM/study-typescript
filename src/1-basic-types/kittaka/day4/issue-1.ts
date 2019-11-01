88a777d5da2d7b04234a8c0b5b6e6a236f43f924
staka121 <staka121@users.noreply.github.com>
2019-11-01 18:24:04 +0900

    [fix] セミコロンとクウォートと改行

--- src/1-basic-types/kittaka/day4/issue-1.ts
+++ src/1-basic-types/kittaka/day4/issue-1.ts
@@ -1,53 +1,51 @@
 interface Data {
-  status: string[]
-  shopName: string
-  categoryName: string
-  ownerName: string
-  sequenceNum: string
-  exportLineId: string
-  dateFrom: string
-  dateTo: string
-  amountFrom: string
-  amountTo: string
-  scope: boolean
-  departmentId: string
-  includeChildDepartment: boolean
+  status: string[];
+  shopName: string;
+  categoryName: string;
+  ownerName: string;
+  sequenceNum: string;
+  exportLineId: string;
+  dateFrom: string;
+  dateTo: string;
+  amountFrom: string;
+  amountTo: string;
+  scope: boolean;
+  departmentId: string;
+  includeChildDepartment: boolean;
 }
 
 export const default_data: Data = {
-  status: ['default', 'waiting_for_worker', 'denied'],
-  shopName: '',
-  categoryName: '',
-  ownerName: '',
-  sequenceNum: '',
-  exportLineId: '',
-  dateFrom: '',
-  dateTo: '',
-  amountFrom: '',
-  amountTo: '',
+  status: ["default", "waiting_for_worker", "denied"],
+  shopName: "",
+  categoryName: "",
+  ownerName: "",
+  sequenceNum: "",
+  exportLineId: "",
+  dateFrom: "",
+  dateTo: "",
+  amountFrom: "",
+  amountTo: "",
   scope: false,
-  departmentId: '',
-  includeChildDepartment: true,
-}
+  departmentId: "",
+  includeChildDepartment: true
+};
 
 class SearchParamsStorage {
   _data: Data;
 
   constructor() {
-    this._data = this.defaultData
+    this._data = this.defaultData;
   }
 
   get data(): Data {
     return this._data || this.defaultData;
   }
-​
   set data(params) {
     this._data = { ...params };
   }
-​
   get defaultData(): Data {
     return default_data;
   }
 }
 
-export default SearchParamsStorage
+export default SearchParamsStorage;
