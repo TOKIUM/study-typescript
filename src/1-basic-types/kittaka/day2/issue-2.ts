b5a92cbc191baeacb86499b3de81f2d239559f34
staka121 <staka121@users.noreply.github.com>
2019-11-01 18:21:30 +0900

    [fix] カンマが嫌いでセミコロンが好きらしい

--- src/1-basic-types/kittaka/day2/issue-2.ts
+++ src/1-basic-types/kittaka/day2/issue-2.ts
@@ -1,15 +1,17 @@
 type Project = {
-  displayId: string,
-  name: string,
-}
+  displayId: string;
+  name: string;
+};
 
 const containSearchText = (
   displayId: string,
   name: string,
-  project: Project,
+  project: Project
 ): boolean => {
-  return (!displayId || (project.displayId.indexOf(displayId) > -1))
-    && (!name || (project.name && project.name.indexOf(name) > -1));
-}
+  return (
+    (!displayId || project.displayId.indexOf(displayId) > -1) &&
+    (!name || (project.name && project.name.indexOf(name) > -1))
+  );
+};
 
 export default containSearchText;
