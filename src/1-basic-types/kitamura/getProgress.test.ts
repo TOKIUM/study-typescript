e43a1e0d3f47d05834f69cc9bc266c2ddd7a0df4
Kitamura Satoshi <kitamura_s@beartail.jp>
2019-11-01 18:11:16 +0900

    dce node yarn lint:fix

--- src/1-basic-types/kitamura/getProgress.test.ts
+++ src/1-basic-types/kitamura/getProgress.test.ts
@@ -5,53 +5,58 @@
  * @param {Array<string>} remaining 配列っぽい？
  * @return {number} 進捗
  */
-function getProgress(finish?: Array<string>, remaining?: Array<string>): number {
+function getProgress(
+  finish?: Array<string>,
+  remaining?: Array<string>
+): number {
   if (!finish || !remaining) {
     return 0;
   }
   const fin: number = finish.length || 0;
   const rem: number = remaining.length || 0;
-​
   switch (true) {
-    case fin <= 0: return 0;
-    case rem <= 0: return 100;
-    default: return parseInt((fin / (fin + rem) * 100).toString(), 10);
+    case fin <= 0:
+      return 0;
+    case rem <= 0:
+      return 100;
+    default:
+      return parseInt(((fin / (fin + rem)) * 100).toString(), 10);
   }
 }
 
-describe('getProgress', () => {
-  test('進捗50%', () => {
-    const finishedApprovals: string[] = ['1', '2', '3'];
-    const remainingApprovals: string[] = ['4', '5', '6'];
+describe("getProgress", () => {
+  test("進捗50%", () => {
+    const finishedApprovals: string[] = ["1", "2", "3"];
+    const remainingApprovals: string[] = ["4", "5", "6"];
 
     expect(getProgress(finishedApprovals, remainingApprovals)).toBe(50);
-  })
+  });
 
-  test('進捗0%', () => {
+  test("進捗0%", () => {
     const finishedApprovals: string[] = [];
-    const remainingApprovals: string[] = ['4', '5', '6'];
+    const remainingApprovals: string[] = ["4", "5", "6"];
 
     expect(getProgress(finishedApprovals, remainingApprovals)).toBe(0);
-  })
+  });
 
-  test('進捗100%', () => {
-    const finishedApprovals: string[] = ['1', '2', '3'];
+  test("進捗100%", () => {
+    const finishedApprovals: string[] = ["1", "2", "3"];
     const remainingApprovals: string[] = [];
 
     expect(getProgress(finishedApprovals, remainingApprovals)).toBe(100);
-  })
+  });
 
-  test('ぬるぽ1', () => {
+  test("ぬるぽ1", () => {
     const finishedApprovals: string[] = null;
-    const remainingApprovals: string[] = ['4', '5', '6'];
+    const remainingApprovals: string[] = ["4", "5", "6"];
 
     expect(getProgress(finishedApprovals, remainingApprovals)).toBe(0);
-  })
+  });
 
-  test('ぬるぽ2', () => {
-    const finishedApprovals: string[] = ['1', '2', '3'];
+  test("ぬるぽ2", () => {
+    const finishedApprovals: string[] = ["1", "2", "3"];
     const remainingApprovals: string[] = null;
 
     expect(getProgress(finishedApprovals, remainingApprovals)).toBe(0);
-  })
-})
+  });
+});
--- src/3-interfaces/kitamura/handleStatusChange.test.ts
+++ src/3-interfaces/kitamura/handleStatusChange.test.ts
@@ -1,16 +1,16 @@
 // app/frontend/javascripts/applications/transactions/transaction_searchbox.jsx
 
 interface SearchCondition {
-  status?: string[]
+  status?: string[];
 }
 
 class SearchBox {
-  searchCondition: SearchCondition
+  searchCondition: SearchCondition;
 
   constructor() {
     this.searchCondition = {
-      status: ['default', 'applied'] // default value
-    }
+      status: ["default", "applied"] // default value
+    };
   }
 
   /**
@@ -18,33 +18,39 @@ class SearchBox {
    * @param {string} value input value
    */
   handleStatusChange(checked: boolean, value: string) {
-    const filtered: string[] = this.searchCondition.status.filter(x => x !== value || checked)
-    const additional: boolean = checked && filtered.indexOf(value) < 0
-    const status: string[] = additional ? [...filtered, value] : filtered
-    this.setSearchConditions({ status })
+    const filtered: string[] = this.searchCondition.status.filter(
+      x => x !== value || checked
+    );
+    const additional: boolean = checked && filtered.indexOf(value) < 0;
+    const status: string[] = additional ? [...filtered, value] : filtered;
+    this.setSearchConditions({ status });
   }
 
   private setSearchConditions(searchCondition: SearchCondition) {
-    this.searchCondition = searchCondition
+    this.searchCondition = searchCondition;
   }
 }
 
-describe('handleStatusChange', () => {
-  test('check default', () => {
-    const searchBox: SearchBox = new SearchBox
-    searchBox.handleStatusChange(true, 'default')
-    expect(searchBox.searchCondition.status).toEqual(['default', 'applied'])
-  })
+describe("handleStatusChange", () => {
+  test("check default", () => {
+    const searchBox: SearchBox = new SearchBox();
+    searchBox.handleStatusChange(true, "default");
+    expect(searchBox.searchCondition.status).toEqual(["default", "applied"]);
+  });
 
-  test('uncheck default', () => {
-    const searchBox: SearchBox = new SearchBox
-    searchBox.handleStatusChange(false, 'default')
-    expect(searchBox.searchCondition.status).toEqual(['applied'])
-  })
+  test("uncheck default", () => {
+    const searchBox: SearchBox = new SearchBox();
+    searchBox.handleStatusChange(false, "default");
+    expect(searchBox.searchCondition.status).toEqual(["applied"]);
+  });
 
-  test('check nyan', () => {
-    const searchBox: SearchBox = new SearchBox
-    searchBox.handleStatusChange(true, 'nyan')
-    expect(searchBox.searchCondition.status).toEqual(['default', 'applied', 'nyan'])
-  })
-})
+  test("check nyan", () => {
+    const searchBox: SearchBox = new SearchBox();
+    searchBox.handleStatusChange(true, "nyan");
+    expect(searchBox.searchCondition.status).toEqual([
+      "default",
+      "applied",
+      "nyan"
+    ]);
+  });
+});
