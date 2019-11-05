d826b550c7cb1e2642de68555a79ef270f0e6ede
staka <staka121@users.noreply.github.com>
2019-11-05 18:56:37 +0900

    wip 猫クラス (#20)
    * かんたんな猫クラス実装
    
    * セミコロン

---
+++ src/1-basic-types/kittaka/day6/issue-1.test.ts
@@ -0,0 +1,25 @@
+import Cat from './issue-1';
+
+describe("Cat", () => {
+  const cat = new Cat('sleeping');
+
+  test("#run", () => {
+    cat.run();
+    expect(cat.getState()).toEqual('running');
+  });
+
+  test("#play", () => {
+    cat.play();
+    expect(cat.getState()).toEqual('playing');
+  });
+
+  test("#eat", () => {
+    cat.eat();
+    expect(cat.getState()).toEqual('eating');
+  });
+
+  test("#sleep", () => {
+    cat.sleep();
+    expect(cat.getState()).toEqual('sleeping');
+  });
+})
