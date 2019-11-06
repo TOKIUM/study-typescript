11f1bf31320f21d90f84a3cbd3ef401f5bbb23ee
西山希 <nishiyama@beartail.jp>
2019-11-06 18:26:32 +0900

    [Add] catNameタイプを実装してみました

--- src/1-basic-types/nishiyama/cat.test.ts
+++ src/1-basic-types/nishiyama/cat.test.ts
@@ -1,26 +1,17 @@
+type catName = 'たま' | 'みけ' | 'わださん';
+
 interface Nyan {
-  name: string;
-  toy: string;
+  name: catName;
 }
 
 class Cat implements Nyan {
-  name: string;
-  toy: string;
-  constructor(name: string, toy: string) {
-    if (!['たま', 'みけ', 'わださん'].includes(name)) {
-      throw new Error();
-    }
+  name: catName;
+  constructor(name: catName) {
     this.name = name;
-    this.toy = toy;
   }
 }
 
 test('valid cat', () => {
-  const cat = new Cat('みけ', '猫じゃらし');
+  const cat = new Cat('みけ');
   expect(cat.name).toEqual('みけ');
-  expect(cat.toy).toEqual('猫じゃらし');
-})
-
-test('invalid cat', () => {
-  expect(new Cat('しば犬まる', 'ほね')).toThrow();
 })
