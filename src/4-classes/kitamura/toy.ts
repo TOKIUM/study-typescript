c60ec05bcf2e29f5900c62477faf58d2ca94d40a
Kitamura Satoshi <34638005+kitamura-beartail@users.noreply.github.com>
2019-11-07 19:08:55 +0900

    いろんなにゃんこを作ってみた (#23)
    * Implement abstract cats
    
    * Add norweigian forest cat
    
    * Add ball to play with a cat

---
+++ src/4-classes/kitamura/toy.ts
@@ -0,0 +1,11 @@
+export abstract class Toy {
+  readonly name: string;
+}
+
+export class Nekojarashi extends Toy {
+  readonly name = 'ねこじゃらし';
+}
+
+export class Ball extends Toy {
+  readonly name = "ボール";
+}
