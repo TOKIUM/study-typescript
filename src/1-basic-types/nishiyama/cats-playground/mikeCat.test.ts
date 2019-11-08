50ce67b1d9895f60a2754a9aac7c19c56c65ad6a
西山希 <nishiyama@beartail.jp>
2019-11-08 18:58:37 +0900

    [Add] 猫が遊びを覚えたにゃん

--- src/1-basic-types/nishiyama/cats-playground/mikeCat.test.ts
+++ src/1-basic-types/nishiyama/cats-playground/mikeCat.test.ts
@@ -1,11 +1,47 @@
 import Cat, { catName } from './cat';
 
+class lovingToy {
+  name: 'ねこじゃらし' | '紙くず';
+
+  constructor(name: 'ねこじゃらし' | '紙くず') {
+    this.name = name;
+  }
+}
+
+class ignoringToy {
+  name: 'ボール' | 'ちゅーる';
+
+  constructor(name: 'ボール' | 'ちゅーる') {
+    this.name = name;
+  }
+}
+
+class Person {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
 class MikeCat extends Cat {
   readonly hairLength: number;
 
   constructor(name: catName) {
     super(name);
     this.hairLength = 10;
+  }
+
+  play(toy: lovingToy, person?: Person): string;
+  play(toy: ignoringToy, person?: Person): string;
+  play(toy, person?): string {
+    if (toy instanceof lovingToy) {
+      return `${toy.name}で遊ぶのたのしいにゃん！！`;
+    } else if (toy instanceof ignoringToy && person && person.name === 'かいぬし') {
+      return `かいぬし、エサくれ！！`;
+    } else {
+      return 'zzz...';
+    }
   }
 }
 
@@ -18,9 +54,12 @@ test('mikeCat', () => {
 
 test('play', () => {
   const mike = new MikeCat('みけ');
-  expect(mike.toy).toBe(null);
-  mike.play('猫じゃらし');
-  expect(mike.toy).toBe('猫じゃらし');
-  expect(() => { mike.play('まり') }).toThrow();
+  const owner = new Person('かいぬし');
+  const nekojarashi = new lovingToy('ねこじゃらし');
+  const ball = new ignoringToy('ボール');
+
+  expect(mike.play(nekojarashi)).toEqual('ねこじゃらしで遊ぶのたのしいにゃん！！');
+  expect(mike.play(ball)).toEqual('zzz...');
+  expect(mike.play(ball, owner)).toEqual('かいぬし、エサくれ！！');
 })
 
