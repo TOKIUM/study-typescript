a4da84b469c24c3e98a4b56fbfe7a88f88c442ec
Hisataka Ishii <scraper.once@gmail.com>
2019-11-12 19:20:53 +0900

    [Add] 猫にんげんを作った (#33)

---
+++ src/1-basic-types/scraperonce/6-generics/CatPerson.ts
@@ -0,0 +1,27 @@
+import ICat from './ICat';
+
+export default class CatPerson<T extends ICat> {
+  constructor(
+    private cat: T
+  ) { }
+
+  sayName(): string {
+    return `なまえは ${this.cat.name} だにゃー`;
+  }
+
+  sayColor(): string {
+    return `毛のいろは ${this.cat.color} だにゃー`;
+  }
+
+  sayHairLength(): string {
+    return `毛のながさは ${this.cat.hairLength} cm だにゃー`;
+  }
+
+  changeCat(cat: T): void {
+    this.cat = cat;
+  }
+
+  trim(length: number): void {
+    this.cat.hairLength = length;
+  }
+}
