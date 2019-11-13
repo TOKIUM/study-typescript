111637dd818e279c79fbc9078a41414f9eec89f1
staka121 <staka121@users.noreply.github.com>
2019-11-13 18:59:59 +0900

    add mymap

---
+++ src/6-generics/kittaka/my_map.ts
@@ -0,0 +1,17 @@
+
+interface Index {
+  id: string,
+}
+
+class MyMap<K extends Index, V> {
+  map: { [index: string] : { subject: K, object: V } }[];
+
+  constructor(init_map: { [index: string] : { subject: K, object: V } }[]) {
+    this.map = init_map || [];
+  }
+
+  // 登録
+  registration(index: K, object:V): void {
+    this.map.push({ [index.id] : { subject: index, object: object } });
+  }
+}
