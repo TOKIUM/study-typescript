8c18fed914e6dfe3462a80c03d5a6e139fed874d
西山希 <nishiyama@beartail.jp>
2019-11-14 19:03:59 +0900

    [Fix] にゃんこ辞典を作ったにゃん

---
+++ src/1-basic-types/nishiyama/6-generics/myMap.ts
@@ -0,0 +1,36 @@
+class MyMap<K, V> {
+  private keys = [] as K[];
+  private values = [] as V[];
+
+  add(key: K, value: V): void {
+    this.keys.push(key);
+    this.values.push(value);
+  }
+
+  remove(key: K): void {
+    const index = this.keys.indexOf(key);
+
+    this.keys = this.keys.filter(function (key, idx) {
+      return idx !== index;
+    })
+    this.values = this.values.filter(function (value, idx) {
+      return idx !== index;
+    })
+  }
+
+  getValueOf(key: K): V {
+    const index = this.keys.indexOf(key);
+
+    if (index === null) {
+      return null;
+    }
+
+    return this.values[index];
+  }
+
+  getCount(): number {
+    return this.keys.length;
+  }
+}
+
+export default MyMap;
