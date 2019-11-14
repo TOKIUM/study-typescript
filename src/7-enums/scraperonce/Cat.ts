519925eca460ac02729e2d45b8ed990f5404ce05
Hisataka Ishii <scraper.once@gmail.com>
2019-11-14 19:26:44 +0900

    [Add] 猫のきもちを考えてみました (#39)

---
+++ src/7-enums/scraperonce/Cat.ts
@@ -0,0 +1,27 @@
+import { HairColor, State, Feeling } from "./enums";
+
+export default class Cat {
+  constructor(
+    public name: string,
+    public color: HairColor,
+    public state: State = State.Awake,
+    public feeling: Feeling = Feeling.Good
+  ) { }
+
+  sleep() {
+    this.state = State.Asleep
+  }
+
+  wake() {
+    this.state = State.Awake
+    this.feeling = Feeling.Bad
+  }
+
+  changeColor(color: HairColor) {
+    this.color = color;
+  }
+
+  eatSomething() {
+    this.feeling = Feeling.Good
+  }
+}
