d24b5620ed282c2667f6ed29561870ac54ab00ac
nozomity <32011005+nozomity@users.noreply.github.com>
2019-11-14 19:27:48 +0900

    [Fix] にゃんこ辞典を作ったにゃん (#36)
    * [Fix] にゃんこ辞典を作ったにゃん
    
    * [Fix] indexOfで値が見つからなかった場合の返り値を間違いました
    
    * [Fix] 指摘事項に対応
    
    * [Fix] 何も返さないようにする

---
+++ src/1-basic-types/nishiyama/6-generics/catDic.test.ts
@@ -0,0 +1,31 @@
+import MyMap from './myMap';
+import MikeCat from '../cats-playground/mikeCat.test';
+import tamaCat from '../cats-playground/tamaCat.test';
+
+test('catDictionary', () => {
+  const catDic = new MyMap<string, MikeCat>();
+
+  expect(catDic.getCount()).toBe(0);
+
+  const mike = new MikeCat('みけ');
+  catDic.add('みけ', mike);
+
+  expect(catDic.getValueOf('みけ')).toBe(mike);
+  expect(catDic.getCount()).toBe(1);
+
+  const tama = new tamaCat('たま');
+  catDic.add('たま', tama);
+
+  expect(catDic.getValueOf('たま')).toBe(tama);
+  expect(catDic.getCount()).toBe(2);
+
+  catDic.remove('たま');
+  expect(catDic.getValueOf('たま')).toBe(null);
+  expect(catDic.getValueOf('みけ')).toBe(mike);
+  expect(catDic.getCount()).toBe(1);
+
+  catDic.remove('みけ');
+  expect(catDic.getValueOf('たま')).toBe(null);
+  expect(catDic.getValueOf('みけ')).toBe(null);
+  expect(catDic.getCount()).toBe(0);
+})
---
+++ src/1-basic-types/nishiyama/6-generics/myMap.ts
@@ -0,0 +1,40 @@
+class MyMap<K, V> {
+  private keys = [] as K[];
+  private values = [] as V[];
+
+  add(key: K, value: V): void {
+    this.keys.push(key);
+    this.values.push(value);
+  }
+
+  remove(key: K): void {
+    const index = this.keys.indexOf(key);
+
+    if (index === -1) {
+      return;
+    }
+
+    this.keys = this.keys.filter((key, idx) => {
+      return idx !== index;
+    })
+    this.values = this.values.filter((value, idx) => {
+      return idx !== index;
+    })
+  }
+
+  getValueOf(key: K): V {
+    const index = this.keys.indexOf(key);
+
+    if (index === -1) {
+      return null;
+    }
+
+    return this.values[index];
+  }
+
+  getCount(): number {
+    return this.keys.length;
+  }
+}
+
+export default MyMap;
