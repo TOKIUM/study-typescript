94108348526a7619326fa36012e526e7bbe66227
Kitamura Satoshi <34638005+kitamura-beartail@users.noreply.github.com>
2019-11-14 19:26:58 +0900

    enumで選択肢を作りました (#37)
    * Implement cat hair color
    
    * Implement cat food

---
+++ src/7-enums/kitamura/CatFood.ts
@@ -0,0 +1,8 @@
+enum CatFood {
+  KaliKali = "カリカリ",
+  TunaCan = "ツナ缶",
+  Sasami = "ささみ",
+  CiaoChuru = "ちゅ～る"
+}
+
+export default CatFood;
---
+++ src/7-enums/kitamura/CatHair.ts
@@ -0,0 +1,8 @@
+enum CatHair {
+  Black = 1,
+  Gold = 2,
+  White = 3,
+  Red = 4
+}
+
+export default CatHair;
