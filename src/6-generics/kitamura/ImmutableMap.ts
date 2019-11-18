85e3d29e9ad0276997f8648db9245cdee9c17a2b
Kitamura Satoshi <kitamura_s@beartail.jp>
2019-11-18 18:53:51 +0900

    Refactor immutable map

--- src/6-generics/kitamura/ImmutableMap.ts
+++ src/6-generics/kitamura/ImmutableMap.ts
@@ -1,24 +1,44 @@
 export default class ImmutableMap<K, V> {
-  protected map: object;
+  protected keys: K[];
+  protected values: V[];
 
-  // objectの型指定したいです
-  constructor(map: object = {}) {
-    this.map = map;
+  constructor() {
+    this.keys = [] as K[];
+    this.values = [] as V[];
   }
 
   has(key: K): boolean {
-    return Object.keys(this.map).includes(key as any); // eslint-disable-line @typescript-eslint/no-explicit-any
+    return this.keys.includes(key);
   }
 
   get(key: K): V {
-    if (this.has(key)) {
-      return this.map[key as any]; // eslint-disable-line @typescript-eslint/no-explicit-any
+    const index = this.keys.indexOf(key);
+
+    if (index === -1) {
+      return null;
     }
 
-    return null;
+    return this.values[index];
   }
 
   set(key: K, value: V): ImmutableMap<K, V> {
-    return new ImmutableMap({ ...this.map, [key as any]: value }); // eslint-disable-line @typescript-eslint/no-explicit-any
+    const index = this.keys.indexOf(key);
+    let keys: K[];
+    let values: V[];
+
+    if (index === -1) {
+      keys = [...this.keys, key];
+      values = [...this.values, value];
+    } else {
+      keys = [...this.keys];
+      values = [...this.values];
+      values[index] = value;
+    }
+
+    const newMap = new ImmutableMap<K, V>();
+    newMap.keys = keys;
+    newMap.values = values;
+
+    return newMap;
   }
 }
