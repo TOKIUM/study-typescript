bb6dc1860418ce8f5c20b3b77cf1f6261ee93a6a
Kitamura Satoshi <kitamura_s@beartail.jp>
2019-11-18 18:30:07 +0900

    Add return type

--- src/greeter.test.ts
+++ src/greeter.test.ts
@@ -14,7 +14,7 @@ interface Person {
   lastName: string;
 }
 
-function greeter(person: Person) {
+function greeter(person: Person): string {
   return "Hello, " + person.firstName + " " + person.lastName;
 }
 
