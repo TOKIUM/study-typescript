307b2b4b6af17c0a46b82ae429807ed391c7bba2
Hisataka Ishii <scraper.once@gmail.com>
2019-11-22 18:53:59 +0900

    typeでMixinをしてみた

--- src/8-advanced-types/scraperonce/sugoi-types/types.test.ts
+++ src/8-advanced-types/scraperonce/sugoi-types/types.test.ts
@@ -33,17 +33,50 @@ describe('types', () => {
     } as Cat<Fish>
 
     const dog = {
-      name: 'たま',
+      name: 'たま（犬）',
       like: meat,
       eat: (food) => `${food.name}は最高だわんにゃん`,
       swim: () => `じゃばじゃば`,
     } as Dog<Namable>;
 
-    type SwimmableAnimal = Extract<Cat<Namable> |Dog<Namable>|Fish, Swimmable>;
+    type SwimmableAnimal = Extract<Cat<Namable> | Dog<Namable> | Fish, Swimmable>;
+  });
 
-    // こんなことがしたい
-    // expect(cat typeof SwimmableAnimal).toBeFalse()
-    // expect(dog typeof SwimmableAnimal).toBeTrue()
-    // expect(fish typeof SwimmableAnimal).toBeTrue()
+  test('kanazuchi fish', () => {
+    type BrickFish = Omit<Fish, keyof Swimmable>;
+
+    const taiyaki: BrickFish = {
+      name: 'たいやき',
+      swim: () => 'とける',
+      tailLength: 20,
+    } as Fish;
+
+    // トランスパイル時にエラーとなる（ですよね）
+    // expect(() => { taiyaki.swim() }).toThrow();
+
+    // でも値としては残るぞ...
+    expect(typeof (taiyaki as any as BrickFish)['swim']).toBe('function');
+  });
+
+  test('mixin', () => {
+
+    // コンストラクタを定義、らしい...
+    type Constructor<T = {}> = new (...args: any[]) => T;
+
+    function Gokigen<T extends Constructor>(Daze: T) {
+      return class extends Daze {
+        loves = 'USA';
+      };
+    }
+
+    class Person {
+      name = '';
+    }
+
+    const GokigenPerson = Gokigen(Person);
+    const issa = new GokigenPerson();
+
+    // Mixin
+    expect(issa.loves).toBe('USA');
   })
 });
