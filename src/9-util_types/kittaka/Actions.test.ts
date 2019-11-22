d61a6515f36e3c62c5f9f0d21fb71072e62fa29f
staka121 <staka121@users.noreply.github.com>
2019-11-22 18:23:48 +0900

    [fix] そもそもテスト落ちてた...

--- src/9-util_types/kittaka/Actions.test.ts
+++ src/9-util_types/kittaka/Actions.test.ts
@@ -2,5 +2,7 @@ import { running, janp, dashJanp, Pos, Direction } from './Actions';
 
 test("running", () => {
   const currentPos: Pos = { x: 0, y: 0 };
-  expect(running(currentPos, "Right", 1)).toBe({ x: 1, y: 0 });
+  const subject = running(currentPos, "Right", 1)
+  expect(subject.x).toBe(1);
+  expect(subject.y).toBe(0);
 });
