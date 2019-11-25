28b4f6f603842222276437b6b78f133a3b55c319
staka <staka121@users.noreply.github.com>
2019-11-25 19:07:20 +0900

    猫の観光名所追加 (#55)
    * [fix] そもそもテスト落ちてた...
    
    * 冒険猫追加
    
    * [fix] 前回分の尻拭い
    
    * [add] 猫の観光名所

--- src/9-util_types/kittaka/Adventurer.ts
+++ src/9-util_types/kittaka/Adventurer.ts
@@ -1,7 +1,6 @@
 export type Adventurer<S, A> = {
   status: S,
   actions?: A & ThisType<S & A>, // S と A を混ぜたもののポインタを返して, そのポインタと A を混ぜる
-
 }
 
 export function makeAdventurer<S, A>(adventurer: Adventurer<S, A>): S & A {
