00ff611429d78a4b8c4a35cb0fc1a4525f5a19e7
Hisataka Ishii <scraper.once@gmail.com>
2019-11-28 19:17:00 +0900

    ちょいてなおし

--- src/10-namespaces/scraperonce/Cats.ts
+++ src/10-namespaces/scraperonce/Cats.ts
@@ -8,25 +8,19 @@ export namespace Cats {
 
 export namespace Cats {
   export class Cat {
-    public name: string;
-    constructor(name: string) {
-      this.name = name;
-    }
+    constructor(public name: string) { }
   }
 }
 
 export namespace Cats {
-  export class MaineCoon extends Cats.Cat {
-  }
+  export class MaineCoon extends Cats.Cat { }
 }
 
 export namespace Cats {
-  export class AmericanShortHair extends Cats.Cat {
-  }
+  export class AmericanShortHair extends Cats.Cat { }
 }
 
 export namespace Cats {
-  export class Abyssinian extends Cats.Cat {
-  }
+  export class Abyssinian extends Cats.Cat { }
 }
 
