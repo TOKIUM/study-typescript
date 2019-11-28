87f8b9a0444c98b4745ea82367e76058c25198b2
hashizumi <hashizumi@beartail.jp>
2019-11-28 19:30:18 +0900

    くまねみ's 猫 (#62)
    * Implement cats
    
    * Fuckin typo

---
+++ src/10-namespaces/hashizumi/catGenerator.ts
@@ -0,0 +1,6 @@
+import * as cats from "./cats";
+let genbaNeko = new cats.GenbaNeko();
+let denwaNeko = new cats.DenwaNeko();
+
+console.log(genbaNeko.nyanText());
+console.log(denwaNeko.nyanText());
